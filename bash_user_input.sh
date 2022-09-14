0b75ef23ba33d60d0847883c654f7cb7e6964676
anyrama <thuany.rama@mgail.com>
2022-09-13 20:12:59 -0400

    uptaded version

--- bash_user_input.sh
+++ bash_user_input.sh
@@ -1,5 +1,23 @@
-#!bin/bash
+#!/bin/bash
 
-echo "what is your name?\n
-read NAME
-echo "yourname is " $NAME
+echo "what is your user_name?"
+read USER_NAME
+
+if [ "$USER_NAME" == "Dave" ]
+then
+	echo "Get off my computer Dave!"
+elif [ "$USER_NAME" == "Angela" ]
+then
+	echo "I know is you Dave, get off my computer!"
+else
+	echo "Invalid user_name"
+fi
+
+CREDITS=120
+
+if [ $CREDITS -ge 120 ]
+then
+	echo "You have enough credits to graduate"
+else
+	echo "You dont have enough credits to graduate"
+fi
